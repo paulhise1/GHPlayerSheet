fb013376d3a4f2d4b1e4cc8ee52db3350a49a141
Paul <paulhise1@gmail.com>
2018-01-15 22:09:32 -0800

    Added name textfield that comes up when the name button is tapped under the character's image in the character sheet.

--- GHPlayerSheet/Controllers/CharacterSheetViewController.swift
+++ GHPlayerSheet/Controllers/CharacterSheetViewController.swift
@@ -6,27 +6,42 @@ class CharacterSheetViewController: UIViewController {
     
     @IBOutlet weak var levelButton: UIButton!
     @IBOutlet weak var characterImage: UIImageView!
+    @IBOutlet weak var enterNameButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var perksLabel: UILabel!
     @IBOutlet weak var notesLabel: UILabel!
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var nameContainer: UIView!
     @IBOutlet weak var stateModifierContainerView: UIView!
     @IBOutlet weak var statModifierContainerLeadingConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupStatModifierView()
+    }
+    
+    
+    @IBAction func enterNameButtonTapped(_ sender: Any) {
+        let textField = UITextField()
+        let width: CGFloat = 100.0
+        let frame = CGRect(x: view.frame.size.width/2-width/2, y: 100, width: width, height: 40)
+        textField.frame = frame
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.3)
+        view.addSubview(textField)
+        textField.returnKeyType = .done
+        textField.becomeFirstResponder()
+    }
+    
+    
+    
+    func setupStatModifierView() {
         if let statModifierView = Bundle.main.loadNibNamed("StatModifierView", owner: self, options: nil)?.first as? StatModifierView {
             stateModifierContainerView.addSubview(statModifierView)
             statModifierView.frame = stateModifierContainerView.bounds
             statModifierView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
             statModifierContainerLeadingConstraint.constant = -(statModifierView.widthForAlignment())
-            
         }
-//        className.text = characterModel?.characterClass.rawValue
-//        characterName.text = characterModel?.name
-//        level.text = String(describing: characterModel!.level)
     }
     
-    //MARK: - Button handling
+    
+    
 }
