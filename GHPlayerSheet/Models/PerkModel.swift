2080eaca4d5aa6cfb5f7e8d9478ca83498ffe458
Paul <paulhise1@gmail.com>
2018-01-25 06:10:38 -0800

    Created files that will be needed to implement perks.  The PerksManager will handle controlling adding perks.  PerkModel will model the Perk object. PerkSelectionView will handle the view that will display from CharacterSheetVC when characters are ready to add a perk.  PerksUnlockedView will be a subview of CharacterSheetVC to display the perks characters have selected for their character.

---
+++ GHPlayerSheet/Models/PerkModel.swift
@@ -0,0 +1,9 @@
+//
+//  PerkModel.swift
+//  GHPlayerSheet
+//
+//  Created by Paul Hise on 1/25/18.
+//  Copyright © 2018 Paul Hise. All rights reserved.
+//
+
+import Foundation
---
+++ GHPlayerSheet/Views/PerkSelection.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class PerkSelection: UIView {
+
+
+
+}
